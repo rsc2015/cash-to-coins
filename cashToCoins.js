8bdb07c9b6fe39204043f616f9a723478ccb6bd4
ROHINI CHAVAN <rohinichavan@ROHINIs-MacBook-Pro.local>
2018-02-02 11:57:30 -0600

    made corrections

--- cashToCoins.js
+++ cashToCoins.js
@@ -4,7 +4,7 @@ console.log("cashToCoins.js");
 //The final result is an object with the correct number of quarters, dimes, nickels, and pennies.
 
 
-const dollarAmount = 1.92;
+const dollarAmount = 1.68;
 const piggyBank = {
     quarters: 0,
     dimes: 0,
@@ -14,6 +14,8 @@ const piggyBank = {
 //convert the cash amount into pennies
 var con_pennies = dollarAmount * 100;
 console.log(con_pennies);
+
+
 
 //count the number of quarters in the change
 if (con_pennies >= 25) {
@@ -26,9 +28,8 @@ if (con_pennies >= 25) {
     //console.log(piggyBank.quarters);
     var remainder_1 = con_pennies - quarters;
     console.log(remainder_1);  
-}
+
 //for the remainder amount, count the number of dimes
-if (remainder_1 >= 10) {
     var sum_Amount_2 = remainder_1 / 10;
     console.log(sum_Amount_2);
     piggyBank.dimes = Math.floor(sum_Amount_2);
@@ -37,9 +38,8 @@ if (remainder_1 >= 10) {
     console.log(dimes);
     var remainder_2 = remainder_1 - dimes;
     console.log(remainder_2);  
-} 
+
 //for the remainder amount, count the number of nickels
-if (remainder_2 >= 5) {
     var sum_Amount_3 = remainder_2 / 5;
     console.log(sum_Amount_3);
     piggyBank.nickels = Math.floor(sum_Amount_3);
@@ -48,9 +48,8 @@ if (remainder_2 >= 5) {
     console.log(nickels);
     var remainder_3 = remainder_2 - nickels;
     console.log(remainder_3);  
-} 
+
 //for the remainder amount, count the number of pennies
-if (remainder_3 >= 1) {
     var sum_Amount_4 = remainder_3;
     console.log(sum_Amount_4);
     piggyBank.pennies = sum_Amount_4;
@@ -59,4 +58,3 @@ if (remainder_3 >= 1) {
 } 
 
 console.log(piggyBank);
-
